a5f3a8d45a01a59dd36cfee5a7127c31a03288c8
tt <xiaotian3303@outlook.com>
2026-02-20 17:12:37 +0800

    今日天气 3-3

--- weather-app/src/components/Home.jsx
+++ weather-app/src/components/Home.jsx
@@ -2,19 +2,32 @@ import { Button } from "@mui/material";
 import Day from "./Day";
 import styles from "./Home.module.css";
 import { getCurrentWeather as getCurrentWeatherApi } from "../services/apiWeather";
+import { useState } from "react";
+import Welcome from "./Welcome";
 
 function Home({ getPosition, status }) {
+  const [data, setData] = useState(null);
+
   async function getCurrentWeather() {
     const position = await getPosition();
-    const data = await getCurrentWeatherApi(
+
+    const crrentWeatherData = await getCurrentWeatherApi(
       position.latitude,
       position.longitude,
     );
-    console.log(data);
+    console.log(crrentWeatherData);
+
+    setData(crrentWeatherData);
   }
   return (
     <section className={styles.section}>
-      <Day />
+      {data && (
+        <Day
+          temperature={{ max: data.main.temp_max, min: data.main.temp_min }}
+          iconCode={data.weather[0].icon}
+        />
+      )}
+      {!data && <Welcome>Welcome To Weather App</Welcome>}
       <Button variant="contained" size="large" onClick={getCurrentWeather}>
         {status}
       </Button>
